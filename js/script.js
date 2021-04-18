0837ccae60f280e677084374198170aa601caffa
Alyssa <alyssaru@umich.edu>
2021-04-18 15:25:13 -0400

    open/close lists

--- js/script.js
+++ js/script.js
@@ -1,4 +1,44 @@
 
 const change = src => {
   document.getElementById('mainImg').src = src
-}
+};
+
+
+document.querySelector("#coachArrow").addEventListener("click", function() {
+  console.log("clicked");
+  toChange = document.querySelector(".coaches");
+  if(toChange.style.visibility=='hidden'){
+    toChange.style.visibility='visible';
+    toChange.style.height='initial';
+    document.querySelector("#coachArrow").src="imgs/uArrow.png"
+  }else{
+    toChange.style.visibility='hidden'; 
+    toChange.style.height=0;
+    document.querySelector("#coachArrow").src="imgs/dArrow.png"}
+});
+
+document.querySelector("#maizeArrow").addEventListener("click", function() {
+  console.log("clicked");
+  toChange = document.querySelector(".maize");
+  if(toChange.style.visibility=='hidden'){
+    toChange.style.visibility='visible';
+    toChange.style.height='initial';
+    document.querySelector("#maizeArrow").src="imgs/uArrow.png"
+  }else{
+    toChange.style.visibility='hidden'; 
+    toChange.style.height=0;
+    document.querySelector("#maizeArrow").src="imgs/dArrow.png"}
+});
+
+document.querySelector("#blueArrow").addEventListener("click", function() {
+  console.log("clicked");
+  toChange = document.querySelector(".blue");
+  if(toChange.style.visibility=='hidden'){
+    toChange.style.visibility='visible';
+    toChange.style.height='initial';
+    document.querySelector("#blueArrow").src="imgs/uArrow.png"
+  }else{
+    toChange.style.visibility='hidden'; 
+    toChange.style.height=0;
+    document.querySelector("#blueArrow").src="imgs/dArrow.png"}
+});
